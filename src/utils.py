0a65765397b7a38f67470c2b87aca446f85cce19
luan <luan.bartole@outlook.com>
2025-07-23 18:41:09 -0300

    feat: create util functions and reorganized folder structure for OOP.

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import json
+
+def save_json(file_path, data):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=1)
+
+def load_json(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return None
